9860f032e3c3e45c0ee184742e8d07731205a8b9
Efim.rozovsky <Efim.rozovsky@operative.com>
2020-07-05 09:13:43 +0300

    Added Publisher class.

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -13,6 +13,10 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
     inverseJoinColumns = @JoinColumn(name="author_id"))
@@ -25,6 +29,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = new HashSet<>();
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Long getId() {
